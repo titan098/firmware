d1ef8c55c323332f7dea104ebc9a71538a97ff00
David Ellefsen <david@ellefsen.co.za>
2024-12-24 08:32:28 +0200

    Add GPSD support for portduino native builds

---
+++ src/gps/GPSDStream.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <libgpsmm.h>
+
+#include "Stream.h"
+#include "configuration.h"
+
+class GPSDStream : public Stream
+{
+  public:
+    GPSDStream(){};
+    ~GPSDStream();
+
+    bool open(const char *host, const char *port);
+
+    int available();
+    int read();
+    int peek();
+
+    // NoOp placeholders to deal with the Stream API
+    size_t write(uint8_t strPtr) { return 0; }
+    size_t write(const char *str) { return 0; }
+    size_t write(uint8_t *str, size_t len) { return 0; }
+    void begin(uint16_t rate){};
+    void end(){};
+
+  private:
+    void refresh();
+
+    gpsmm *_gpsd = nullptr;
+    struct gps_data_t *_gps_data = nullptr;
+    const char *_gps_buffer = nullptr;
+    uint32_t _last_refresh = 0;
+
+    String _buffer;
+};
